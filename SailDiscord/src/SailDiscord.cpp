163918fa02fa529643c4cdba2972bd8437998857
roundedrectangle <kozelderezel12345678@gmail.com>
2023-06-16 18:17:05 +0300

    Remove unnecessary stuff

--- SailDiscord/src/SailDiscord.cpp
+++ SailDiscord/src/SailDiscord.cpp
@@ -10,16 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/SailDiscord.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     app->setApplicationName("saildiscord");
     app->setOrganizationDomain("io.github.roundedrectangle");
@@ -30,6 +20,4 @@ int main(int argc, char *argv[])
     view->show();
 
     return app->exec();
-
-    //return SailfishApp::main(argc, argv);
 }
