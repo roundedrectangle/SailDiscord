17bae9df5ddb8ae2562e346e3a18f6d23994bccd
roundedrectangle <kozelderezel12345678@gmail.com>
2024-07-29 21:39:02 +0300

    Make send_message function

--- SailDiscord/qml/python/communicator.py
+++ SailDiscord/qml/python/communicator.py
@@ -49,6 +49,9 @@ def send_channels_no_category(guild, user_id):
                 has_permissions = c.permissions_for(member).view_channel
             pyotherside.send(f'channel{c.guild.id} -1', str(c.id), str(c.name), has_permissions, str(getattr(getattr(c, 'type'), 'name')))
 
+def send_message(message):
+    # Ironically, this is for incoming messages (or already sent messages by you or anyone else in the past).
+    pyotherside.send('message', str(message.guild.id), str(message.channel.id), str(message.id), str(message.author), str(message.content))
 
 
 class MyClient(discord.Client):
@@ -67,7 +70,7 @@ class MyClient(discord.Client):
             return
         if message.guild.id == self.current_server.id and message.channel.id == self.current_channel.id:
             #pyotherside.send(f"Got message from {message.author} in server {message.guild.name}: {message.content}")
-            pyotherside.send('message', str(message.guild.id), str(message.channel.id), str(message.id), str(message.author), str(message.content))
+            send_message(message)
             #await message.channel.send('pong')
 
     def set_current_channel(self, guild, channel):
