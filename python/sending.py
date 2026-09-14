8894188ac85621a49f424b2023e4f993c922d238
roundedrectangle <roundedrectangle0@gmail.com>
2026-09-14 11:51:31 +0300

    Fix error on message snapshots with mentions

--- python/sending.py
+++ python/sending.py
@@ -192,8 +192,9 @@ def generate_extra_message(message: discord.Message | discord.MessageSnapshot, c
         return 'newmember', ()
 
     content = message.content.replace('<', '\\<')
-    content = emojify(content, cacher, emoji_size, CUSTOM_EMOJI_RE_ESCAPED, 1) if isinstance(message, discord.Message) else content
-    content = escape_mentions(content, message)
+    if isinstance(message, discord.Message):
+        content = emojify(content, cacher, emoji_size, CUSTOM_EMOJI_RE_ESCAPED, 1)
+        content = escape_mentions(content, message)
 
     if t in (discord.MessageType.default, discord.MessageType.reply):
         return 'message', (message.content, content, ref or {})
